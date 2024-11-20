272a04f3dc3b305d41a9b42050f3d906f0c52ac3
jiayunlee <lee.cammi109@gmail.com>
2024-11-20 21:55:03 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Cammi1009',
+    password: 'mysql',
     database: 'breakfast_system'
 });
 
